15cfb7f8500298339b708b23d2a1979918fbc644
Dr-Positive <116766276+Gokegold@users.noreply.github.com>
2023-05-29 17:59:48 +0100

    Update GeesForGeeks.py
    https://pynative.com/python-object-oriented-programming-oop-exercise/
    and 
    https://www.geeksforgeeks.org/maximum-of-two-numbers-in-python/

--- GeesForGeeks.py
+++ GeesForGeeks.py
@@ -1,5 +1,5 @@
 """
-OOP_test.py
+description: Greeksforgeeks test
 
 Twitter @i_amgoke: https://twitter.com/i_amgoke
 
@@ -7,7 +7,7 @@ Github: https://www.github.com/Gokegold
 
 Date Created: May 28, 2023
 
-last modification:: [May 28, 2023]
+last modification:: [May 28, 2023], [May, 28 2023]
 
 """
 
@@ -108,3 +108,132 @@ numbs_2 = 12
 
 sums = operator.add(numbs_1, numbs_2)
 print(f"the sums of {numbs_2} and {numbs_1} is {sums}")
+# OR
+return f"the sum of{numbs_2} and {numbs_1} is {sums}"
+
+      
+print(sums)
+
+
+# question Q
+"""
+https://pynative.com/python-object-oriented-programming-oop-exercise/
+
+Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+"""
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage, colour="White"):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+        self.colour = colour
+
+
+class Bus(Vehicle):
+    def bus_details(self):
+        return f"Colour: {self.colour}, Vehicle name: {self.name}, Speed: {self.max_speed}, Mileage: {self.mileage}"
+
+
+class Car(Vehicle):
+    def bus_details(self):
+        return f"Colour: {self.colour}, Vehicle name: {self.name}, Speed: {self.max_speed}, Mileage: {self.mileage}"
+
+
+buses = Bus("School Volvo", 180, 12)
+cars = Car("Audi Q5", 240, 18)
+
+print(Bus.bus_details(buses))
+print(Car.bus_details(cars))
+
+"""
+https://pynative.com/python-object-oriented-programming-oop-exercise/
+
+OOP Exercise 4: Class Inheritance
+Given:
+
+Create a Bus class that inherits from the Vehicle class.
+Give the capacity argument of Bus.seating_capacity() a default value of 50.
+"""
+# question Q1
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+
+class Bus(Vehicle):
+    # assign default value to capacity
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+    # to print or return the capacity of the child class
+    # with the default value of 50
+    # we have to call the attribute or the instance in the child class method
+    # into the overriden method the will we be able to call the attribute with the 50 capacity.
+
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.seating_capacity())
+
+
+# Question 6
+"""
+Given two numbers, write a Python code to find the Maximum of these two numbers.
+"""
+num_1 = input("Enter a number: ")
+num_2 = input("Enter a number: ")
+
+if num_1 >= num_2:
+    print(f"{num_1}, is greater and equals to {num_2}")
+else:
+    print(f"{num_1}, is less than {num_2}")
+
+# OR
+
+
+def maximum(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+
+numbs = maximum(5, 2)
+print(numbs)
+
+
+# OR
+
+def maxi(y, z):
+    if y >= z:
+        return y
+    else:
+        return z
+
+
+print(maxi(2, 4))
+
+# OR
+
+
+def maxim(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+
+print(maxim(5, 2))
+
+# OR
+
+c = 11
+d = 2
+
+max_number = max(c, d)
+print(max_number)
